d3e79153a94676e2fc41b2405c3ad91909777714
並木裕介 <namikiyuusuke@namikiyuusukenoMacBook-Pro.local>
2024-02-29 22:07:26 +0900

    management4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Pagination\Paginator;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -16,9 +18,11 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Paginator::useBootstrap();
     }
 }
